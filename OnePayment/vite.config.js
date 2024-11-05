0b3cb0053c19971aa7b227d389ebbb36e0e9075b
james <pattanasinpluksek@gmail.com>
2024-11-05 14:28:26 +0700

    update

--- OnePayment/vite.config.js
+++ OnePayment/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import process from "process";
-import util from 'util';
 
 export default defineConfig({
-  define: {
-    "process.env": process.env,
-  },
   plugins: [react()],
+  base: "/Payment-Project/", 
 });
