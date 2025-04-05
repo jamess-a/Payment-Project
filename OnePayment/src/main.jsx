48cc3b2d329820bd30ad0b9b261f1e083868162f
art <pattanasinpluksek@gmail.com>
2025-04-05 11:22:32 +0700

    feat: update fav&font

--- OnePayment/src/main.jsx
+++ OnePayment/src/main.jsx
@@ -1,14 +1,23 @@
 import { StrictMode } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "Kanit, sans-serif",
+  },
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <App />
-    </LocalizationProvider>
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <App />
+      </LocalizationProvider>
+    </ThemeProvider>
   </StrictMode>
 );
